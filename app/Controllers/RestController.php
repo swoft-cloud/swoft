bdc9de99d149ff145ed6e0f38bf5efc03a2f4a7c
lilin <794774870@qq.com>
2017-11-14 22:26:47 +0800

    整理结构

--- app/Controllers/RestController.php
+++ app/Controllers/RestController.php
@@ -3,22 +3,23 @@
 namespace App\Controllers;
 
 use Swoft\Bean\Annotation\Ary;
-use Swoft\Bean\Annotation\EnumFlt;
+use Swoft\Bean\Annotation\AutoController;
+use Swoft\Bean\Annotation\EnumFloat;
 use Swoft\Bean\Annotation\EnumInteger;
 use Swoft\Bean\Annotation\EnumNumber;
-use Swoft\Bean\Annotation\EnumStr;
-use Swoft\Bean\Annotation\Flt;
+use Swoft\Bean\Annotation\EnumString;
+use Swoft\Bean\Annotation\Floats;
 use Swoft\Bean\Annotation\Integer;
 use Swoft\Bean\Annotation\Number;
 use Swoft\Bean\Annotation\RequestMapping;
 use Swoft\Bean\Annotation\RequestMethod;
-use Swoft\Bean\Annotation\Str;
+use Swoft\Bean\Annotation\Strings;
 use Swoft\Web\Controller;
-use Swoft\Web\Request;
-use Swoft\Web\Response;
 
 /**
  * restful和参数验证测试demo
+ *
+ * @AutoController(prefix="/users")
  *
  * @uses      RestController
  * @version   2017年11月13日
@@ -29,7 +30,10 @@ use Swoft\Web\Response;
 class RestController extends Controller
 {
     /**
+     * 查询列表接口
+     * 地址:/users
      *
+     * @RequestMapping(route="", method={RequestMethod::GET})
      */
     public function actionList()
     {
@@ -37,19 +41,65 @@ class RestController extends Controller
     }
 
     /**
-     * @RequestMapping(route="/{$name}", method={RequestMethod::POST})
+     * 创建一个用户
+     * 地址:/users
      *
-     * @Str(name="string", min=1, min=6, default="str")
-     * @Flt(name="float", min=1.1, max=1.6, default="1.3")
+     * @RequestMapping(route="", method={RequestMethod::POST, RequestMethod::PUT})
+     *
+     * @Strings(name="string", min=1, min=6, default="str")
+     * @Floats(name="float", min=1.1, max=1.6, default="1.3")
      * @Integer(from="get/post/body", name="integer", min=-1, max=6, default="3")
      * @Number(name="number", min=3, max=9, default="5")
-     * @EnumStr(name="enumString", values={"a", "b"}, default="b")
-     * @EnumFlt(name="enumFloat", values={1.1, 1.2}, default="1.1")
+     * @EnumString(name="enumString", values={"a", "b"}, default="b")
+     * @EnumFloat(name="enumFloat", values={1.1, 1.2}, default="1.1")
      * @EnumInteger(name="enumInteger", values={-1,3,-9}, default="9")
      * @EnumNumber(name="enumNumber", values={1,2,4,6}, default="8")
-     * @Ary(name="array", delimiter="," min=1, max=10, default="a,b")
      */
-    public function actionCreate(Request $request, Response $response, string $name, \ArrayObject $params)
+    public function actionCreate()
+    {
+
+    }
+
+    /**
+     * 查询一个用户信息
+     * 地址:/user/6
+     *
+     * @RequestMapping(route="{uid}", method={RequestMethod::GET})
+     */
+    public function actionGetUser(int $uid)
+    {
+
+    }
+
+    /**
+     * 查询用户的书籍信息
+     * 地址:/user/6/book/8
+     *
+     * @RequestMapping(route="{userId}/book/{bookId}", method={RequestMethod::GET})
+     */
+    public function actionGetBookFromUser(int $userId, $bookId)
+    {
+
+    }
+
+    /**
+     * 删除一个用户信息
+     * 地址:/user/6
+     *
+     * @RequestMapping(route="{uid}", method={RequestMethod::DELETE})
+     */
+    public function actionDeleteUser(int $uid)
+    {
+
+    }
+
+    /**
+     * 更新一个用户信息
+     * 地址:/user/6
+     *
+     * @RequestMapping(route="{uid}", method={RequestMethod::PATCH})
+     */
+    public function actionUpdateUser(int $uid)
     {
 
     }
