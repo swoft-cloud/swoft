5193a9db770cbbe1ef5e3386055f4eb45c9d8601
huangzhhui <huangzhwork@gmail.com>
2017-11-30 21:06:05 +0800

    增加 DemoControllerTest

---
+++ test/Web/RedisControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Web;
+
+use Swoft\Test\Web\AbstractTestCase;
+
+
+/**
+ * @uses      RedisControllerTest
+ * @version   2017年11月30日
+ * @author    huangzhhui <huangzhwork@gmail.com>
+ * @copyright Copyright 2010-2017 Swoft software
+ * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
+ */
+class RedisControllerTest extends AbstractTestCase
+{
+
+    /**
+     * @test
+     * @requires extension redis
+     * @covers \App\Controllers\RedisController
+     */
+    public function actionTest()
+    {
+        $expected = [
+            'setResult' => true,
+            'getResult' => 123321
+        ];
+        $response = $this->request('GET', '/redis/test', [], parent::ACCEPT_JSON);
+        $response->assertSuccessful()->assertJson($expected);
+    }
+
+}
