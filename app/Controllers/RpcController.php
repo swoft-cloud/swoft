02cdb43565f15162ed0c5162931060a0e66a2cf8
lilin <794774870@qq.com>
2017-12-10 23:35:20 +0800

    新增rpc中间件

--- app/Controllers/RpcController.php
+++ app/Controllers/RpcController.php
@@ -36,4 +36,25 @@ class RpcController
         $result = Service::call("user", 'User::getUser', [1,2,'boy', '1.3']);
         return ['validator', $result];
     }
+
+
+    /**
+     * @RequestMapping("pm")
+     */
+    public function parentMiddleware()
+    {
+        $result = Service::call("user", 'Md::pm');
+
+        return ['validator', $result];
+    }
+
+    /**
+     * @RequestMapping("fm")
+     */
+    public function funcMiddleware()
+    {
+        $result = Service::call("user", 'Md::fm');
+
+        return ['validator', $result];
+    }
 }
