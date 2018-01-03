0c37b2d991e46f6ef0405815b8795b25e9b8167f
lilin <794774870@qq.com>
2018-01-03 21:01:11 +0800

    redis新增db选择

--- config/properties/cache.php
+++ config/properties/cache.php
@@ -1,17 +1,18 @@
 <?php
 return [
     'redis' => [
-        'name' => 'redis',
+        'name'        => 'redis',
         "uri"         => [
             '127.0.0.1:6379',
             '127.0.0.1:6379',
         ],
-        "maxIdel"     => 8,
-        "maxActive"   => 8,
-        "maxWait"     => 8,
-        "timeout"     => 8,
-        "balancer"    => 'random',
-        "useProvider" => false,
+        'maxIdel'     => 8,
+        'maxActive'   => 8,
+        'maxWait'     => 8,
+        'timeout'     => 8,
+        'balancer'    => 'random',
+        'useProvider' => false,
         'provider'    => 'consul',
+        'db'          => 1,
     ],
 ];
