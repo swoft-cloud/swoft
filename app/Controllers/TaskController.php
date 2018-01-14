a11d4f568589422e6504abf1c16ffc4503804c35
lilin <794774870@qq.com>
2018-01-14 10:58:45 +0800

    add excpetion

--- app/Controllers/TaskController.php
+++ app/Controllers/TaskController.php
@@ -18,6 +18,6 @@ class TaskController
     public function cor()
     {
         $result  = Task::deliver('test', 'corTask', ['params1', 'params2'], Task::TYPE_COR);
-        return $result;
+        return [$result, 1];
     }
 }
