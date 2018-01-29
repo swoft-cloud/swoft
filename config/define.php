aae4995b4d1eca929f701c311661f30341dd7998
huangzhhui <huangzhwork@gmail.com>
2018-01-29 17:49:40 +0800

    translate to English

--- config/define.php
+++ config/define.php
@@ -4,14 +4,14 @@ use \Swoft\App;
 
 // Constants
 !defined('DS') && define('DS', DIRECTORY_SEPARATOR);
-// 系统名称
+// App name
 !defined('APP_NAME') && define('APP_NAME', 'swoft');
-// 基础根目录
+// Project base path
 !defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
-// cli命名空间
+// Cli namespace
 !defined('COMMAND_NS') && define('COMMAND_NS', "App\Commands");
 
-// 注册别名
+// Register alias
 $aliases = [
     '@root'       => BASE_PATH,
     '@app'        => '@root/app',
