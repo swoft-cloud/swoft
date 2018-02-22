f1aaa4e524003c973d0030b37816c372fed3e717
lilin <794774870@qq.com>
2018-02-22 09:26:08 +0800

    rm desc

--- app/Commands/TestCommand.php
+++ app/Commands/TestCommand.php
@@ -12,14 +12,9 @@ use Swoft\Core\Coroutine;
 use Swoft\Log\Log;
 
 /**
- * the group of test command
+ * Test command
  *
  * @Command(coroutine=true)
- * @uses      TestCommand
- * @version   2017年11月03日
- * @author    stelin <phpcrazy@126.com>
- * @copyright Copyright 2010-2016 swoft software
- * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
  */
 class TestCommand
 {
