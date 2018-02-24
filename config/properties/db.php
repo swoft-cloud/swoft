c7f9df19619a2b35f77f71aa8efe634ae1910d61
lilin <794774870@qq.com>
2018-02-24 14:26:21 +0800

    fix bug

--- config/properties/db.php
+++ config/properties/db.php
@@ -3,8 +3,8 @@ return [
     'master' => [
         'name'        => 'master',
         'uri'         => [
-            '127.0.0.1:3306',
-            '127.0.0.1:3306',
+            '127.0.0.1:3306/test?user=root&password=123456&charset=utf8',
+            '127.0.0.1:3306/test?user=root&password=123456&charset=utf8',
         ],
         'maxIdel'     => 8,
         'maxActive'   => 8,
@@ -18,8 +18,8 @@ return [
     'slave' => [
         'name'        => 'slave',
         'uri'         => [
-            '127.0.0.1:3306',
-            '127.0.0.1:3306',
+            '127.0.0.1:3306/test?user=root&password=123456&charset=utf8',
+            '127.0.0.1:3306/test?user=root&password=123456&charset=utf8',
         ],
         'maxIdel'     => 8,
         'maxActive'   => 8,
