2ac18f0137759baffb94f367c99f64ffbad03aff
huangzhhui <huangzhwork@gmail.com>
2018-02-26 02:15:53 +0800

    add latest HTTPClient use cases

---
+++ app/Controllers/HttpClientController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use Swoft\Http\Client;
+use Swoft\Http\Server\Bean\Annotation\Controller;
+
+/**
+ * @Controller(prefix="/httpClient")
+ */
+class HttpClientController
+{
+    /**
+     * @return array
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
+    public function request(): array
+    {
+        $client = new Client();
+        $result = $client->get('https://www.swoft.org')->getResult();
+        $result2 = $client->get('https://www.swoft.org')->getResponse()->getBody()->getContents();
+        return compact('result', 'result2');
+    }
+}
