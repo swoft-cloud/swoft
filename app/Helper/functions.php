56b1e8ade9632140efab54fb3dbbfe9bdee2243b
Inhere <inhere@users.noreply.github.com>
2018-10-22 13:34:07 +0800

    Create functions.php

---
+++ app/Helper/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact group@swoft.org
+ * @license https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+ 
+ // you can add some custom functions.
+ 
