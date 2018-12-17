002f9de67e64912464f9e34067cde21c87155c29
Wfuren <wfuren@qq.com>
2018-12-17 18:29:31 +0800

    修改全局函数问题
    不能排除扫描Helper不然方法不被加载会不存在

--- app/Helper/Functions.php
+++ app/Helper/Functions.php
@@ -8,5 +8,9 @@
  * @license https://github.com/swoft-cloud/swoft/blob/master/LICENSE
  */
  
- // you can add some custom functions.
- 
+ // 全局函数需要加上function_exists判断，不然多次回调会导致重复定义（不知道这里备注对不对，不对请修正）
+if (!function_exists('demo')) {
+    function demo(){
+        return 123;
+    }
+}
