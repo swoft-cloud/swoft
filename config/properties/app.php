d015a5c84d872466085d276c44d7fb1a7110af86
Wfuren <wfuren@qq.com>
2018-12-17 18:31:17 +0800

    修改全局函数配置问题
    不能在此处排除Helper目录不然会导致文件不被加载，函数无法使用

--- config/properties/app.php
+++ config/properties/app.php
@@ -17,7 +17,7 @@ return [
         'App\Boot',
     ],
     'excludeScan'  => [
-        'App\Helper',
+        
     ],
     'I18n'         => [
         'sourceLanguage' => '@resources/languages/',
