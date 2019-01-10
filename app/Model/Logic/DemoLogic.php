563db3eb4e8e92912ab4eef058b5422618494c4a
stelin <794774870@qq.com>
2019-01-10 17:20:35 +0800

    parse object definition by annotation and definition

--- app/Model/Logic/DemoLogic.php
+++ app/Model/Logic/DemoLogic.php
@@ -7,7 +7,7 @@ use Swoft\Bean\Annotation\Mapping\Bean;
 use Swoft\Bean\Annotation\Mapping\Inject;
 
 /**
- * @Bean()
+ * @Bean(alias="deloc")
  */
 class DemoLogic
 {
@@ -16,4 +16,55 @@ class DemoLogic
      * @var DemoData
      */
     private $data;
+
+    /**
+     * @var string
+     */
+    private $definitionData;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var int
+     */
+    private $type = 0;
+
+    /**
+     * @var DemoData
+     */
+    private $construnctData;
+
+    /**
+     * DemoLogic constructor.
+     *
+     * @param string $name
+     * @param string $type
+     * @param DemoData
+     */
+    public function __construct(string $name, string $type, DemoData $data)
+    {
+        $this->name = $name;
+        $this->type = $type;
+
+        $this->construnctData = $data;
+    }
+
+    /**
+     * @return DemoData
+     */
+    public function getData(): DemoData
+    {
+        return $this->data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDefinitionData(): string
+    {
+        return $this->definitionData;
+    }
 }
--- app/bean.php
+++ app/bean.php
@@ -1,4 +1,11 @@
 <?php
 return [
-
+    \App\Model\Logic\DemoLogic::class => [
+        [
+            'dDname',
+            12,
+            '${\App\Model\Data\DemoData}'
+        ],
+        'definitionData' => 'definitionData...'
+    ]
 ];
