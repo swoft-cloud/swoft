cfff28c9c24fadd908538580aea147d143f2c162
张城铭 <zhangchengming@kkguan.com>
2019-03-06 14:57:45 +0800

    fix

--- app/WebSocket/ChatModule.php
+++ app/WebSocket/ChatModule.php
@@ -4,22 +4,35 @@ namespace App\WebSocket;
 
 use Swoft\WebSocket\Server\Annotation\Mapping\WsModule;
 use Swoft\WebSocket\Server\Contract\WsModuleInterface;
-use Swoole\Http\ServerRequest;
+use Swoole\Http\Request;
 use Swoole\Http\Response;
-use Swoole\WebSocket\Frame;
-use Swoole\WebSocket\Server;
 use Swoft\WebSocket\Server\MessageParser\JsonParser;
-use Swoft\WebSocket\Server\Annotation\Mapping\OnClose;
-use Swoft\WebSocket\Server\Annotation\Mapping\OnHandShake;
-use Swoft\WebSocket\Server\Annotation\Mapping\OnOpen;
 
 /**
  * Class AbstractModule
  * @since 2.0
  *
- * @WsModule(path="/chat", messageParser=JsonParser::class)
+ * @WsModule(name="chat", path="/chat", messageParser=JsonParser::class)
  */
 class ChatModule implements WsModuleInterface
 {
     //
+    /**
+     * 在这里你可以验证握手的请求信息
+     * - 必须返回含有两个元素的array
+     *  - 第一个元素的值来决定是否进行握手
+     *  - 第二个元素是response对象
+     * - 可以在response设置一些自定义header,body等信息
+     * @param Request $request
+     * @param Response $response
+     * @return array
+     * [
+     *  self::HANDSHAKE_OK,
+     *  $response
+     * ]
+     */
+    public function checkHandshake(Request $request, Response $response): array
+    {
+        return [self::ACCEPT, $response];
+    }
 }
