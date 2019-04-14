f2ee008f86696b2a12b375e4867e36dec44245ec
stelin <794774870@qq.com>
2019-04-14 19:57:33 +0800

    Added ab test

--- bin/test.php
+++ bin/test.php
@@ -1,23 +1,3 @@
 <?php
-$r = new \ReflectionClass(\Redis::class);
 
-$cms = [];
-foreach ($r->getMethods() as $method) {
-    $cms[] = strtolower($method->getName());
-}
-
-//var_dump($cms);
-
-$rc = new \ReflectionClass(\RedisCluster::class);
-
-$rcs = [];
-foreach ($rc->getMethods() as $method) {
-    $rcs[] = strtolower($method->getName());
-}
-//var_dump(count($rcs));
-
-echo '[';
-foreach (array_intersect($cms, $rcs) as $a){
-    echo "'$a',".PHP_EOL;
-}
-echo ']';
+system('ab -n 10000 -c 2000  127.0.0.1:18306/redis/et > /dev/null');
