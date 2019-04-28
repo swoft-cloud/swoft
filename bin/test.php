2f3b685cd112339a11f09443f27d3fd50eeedd0d
daydaygo <1252409767@qq.com>
2019-04-28 11:35:46 +0800

    fix

--- bin/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$fruits = array("apple","banana","pear");
-$numbered = array("1","2","3","pear");
-$cards = array_merge($fruits, $numbered);
-print_r($cards);
-print_r(array_unique($cards));
