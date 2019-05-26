e6f08c7de0a7f3ae71f47f470f0093cd5bf6750c
stelin <794774870@qq.com>
2019-05-26 19:50:25 +0800

    add bean

--- app/Console/Command/TestCommand.php
+++ app/Console/Command/TestCommand.php
@@ -88,6 +88,9 @@ class TestCommand
             ],
             'co' => [
                 '/co/multi'
+            ],
+            'bean' => [
+                '/bean/request'
             ]
         ];
     }
