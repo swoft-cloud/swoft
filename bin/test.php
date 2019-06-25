3a0f75397ab3d7422c207170b4d8271eff792660
stelin <794774870@qq.com>
2019-06-25 11:06:35 +0800

    Fixed dev.composer.json

--- bin/test.php
+++ bin/test.php
@@ -1,12 +1,84 @@
 <?php
 
-$a = [
-    'a' => '1',
-    'b' => '2'
-];
+use Swoft\Bean\Annotation\Mapping\Bean;
+use Swoft\Bean\Annotation\Mapping\Inject;
+use Swoft\Bean\Annotation\Mapping\Primary;
+use Swoft\Bean\BeanFactory;
 
-echo '----'.current($a);
+interface SmsInterface
+{
+    public function send(string $content): bool;
+}
 
-var_dump(key($a));
 
-var_dump($a);
+/**
+ * Class AliyunSms
+ *
+ * @since 2.0
+ *
+ * @Bean()
+ * @Primary()
+ */
+class AliyunSms implements SmsInterface
+{
+    /**
+     * @param string $content
+     *
+     * @return bool
+     */
+    public function send(string $content): bool
+    {
+        return true;
+    }
+}
+
+/**
+ * Class QcloudSms
+ *
+ * @since 2.0
+ *
+ * @Bean()
+ */
+class QcloudSms implements SmsInterface
+{
+    /**
+     * @param string $content
+     *
+     * @return bool
+     */
+    public function send(string $content): bool
+    {
+        return true;
+    }
+}
+
+/**
+ * Class Sms
+ *
+ * @since 2.0
+ *
+ * @Bean()
+ */
+class Sms implements SmsInterface
+{
+    /**
+     * @Inject()
+     *
+     * @var SmsInterface
+     */
+    private $smsInterface;
+
+    /**
+     * @param string $content
+     *
+     * @return bool
+     */
+    public function send(string $content): bool
+    {
+        return $this->smsInterface->send($content);
+    }
+}
+
+/* @var SmsInterface $sms*/
+$sms = BeanFactory::getBean(Sms::class);
+$sms->send('sms content');
