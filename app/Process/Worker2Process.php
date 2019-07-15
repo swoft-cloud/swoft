0f9ce68212e1cba7c397847002c510e682e00a08
stelin <794774870@qq.com>
2019-07-15 20:29:41 +0800

    Add process pool demo

---
+++ app/Process/Worker2Process.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Process;
+
+
+use Swoft\Log\Helper\CLog;
+use Swoft\Process\Annotation\Mapping\Process;
+use Swoft\Process\Contract\ProcessInterface;
+use Swoole\Coroutine;
+use Swoole\Process\Pool;
+
+/**
+ * Class Worker2Process
+ *
+ * @since 2.0
+ *
+ * @Process(workerId=2)
+ */
+class Worker2Process implements ProcessInterface
+{
+    /**
+     * @param Pool $pool
+     * @param int  $workerId
+     */
+    public function run(Pool $pool, int $workerId): void
+    {
+        while (true){
+            CLog::info('worker-2');
+
+            Coroutine::sleep(3);
+        }
+    }
+}
