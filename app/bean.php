0cd11d8b0975d23f2a49293e91244f145a230187
stelin <794774870@qq.com>
2019-07-17 20:37:50 +0800

    Add lua pre

--- app/bean.php
+++ app/bean.php
@@ -84,6 +84,9 @@ return [
         'host'     => '127.0.0.1',
         'port'     => 6379,
         'database' => 0,
+        'option' => [
+            'prefix' => 'swoft:prefix:'
+        ]
     ],
     'user'           => [
         'class'   => ServiceClient::class,
