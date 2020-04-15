b8f1e7a4036e98389533a1b2984df32c9e862d09
inhere <in.798@qq.com>
2020-04-15 14:21:53 +0800

    fix phpunit error

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -17,8 +17,6 @@ $vendor  = dirname(__DIR__) . '/vendor';
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 $loader->addPsr4('SwoftTest\\Testing\\', $vendor . '/swoft/framework/test/testing/');
 
-$application = new TestApplication([
-    'basePath' => $baseDir,
-]);
+$application = new TestApplication($baseDir);
 $application->setBeanFile($baseDir . '/app/bean.php');
 $application->run();
